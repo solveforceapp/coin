8b2366bff8e524a330c5940d01598381b9cc9e41
Ronald Joseph Legarski, Jr. <98552991+solveforceapp@users.noreply.github.com>
2025-06-29 11:44:21 -0700

    Fix parsing exception handling in util test_runner

--- test/util/test_runner.py
+++ test/util/test_runner.py
@@ -121,13 +121,23 @@ def bctest(test_dir, test_obj, buildenv):
         # Parse command output and expected output
         try:
             a_parsed = parse_output(res.stdout, output_type)
-
-            logging.error("Error parsing command output as %s: '%s'; res: %s", output_type, str(e), str(res))
+        except (ValueError, NotImplementedError) as e:
+            logging.error(
+                "Error parsing command output as %s: '%s'; res: %s",
+                output_type,
+                str(e),
+                str(res),
+            )
             raise
         try:
             b_parsed = parse_output(output_data, output_type)
-
-            logging.error("Error parsing expected output %s as %s: %s", output_fn, output_type, e)
+        except (ValueError, NotImplementedError) as e:
+            logging.error(
+                "Error parsing expected output %s as %s: %s",
+                output_fn,
+                output_type,
+                e,
+            )
             raise
         # Compare data
         if a_parsed != b_parsed:
